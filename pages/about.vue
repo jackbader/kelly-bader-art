47a4bf6f34e4bcdcc8caaa55a643cdca8516e40b
Jack Bader <jack@culpritunderwear.com>
2024-04-17 15:47:18 -0400

    updating about page

--- pages/about.vue
+++ pages/about.vue
@@ -18,25 +18,18 @@
     >
       <h1 class="text-4xl font-bold mb-4">About Me</h1>
       <p class="text-sm max-w-xl">
-        Kelly grew up in a very country setting in Ohio. Her and Huck Finn
-        would’ve been best friends! She loved the river, the woods, exploring
-        every day and finding treasures wherever her day took her! She comes
-        from a family of artists; her mom gave her the love of art and it was
-        always in the back of her mind somewhere hiding.<br /><br />
-        Fast forward to 2018 and that’s when she decided to pick up a paintbrush
-        for the very first time! No real formal training, she decided to enroll
-        in a local painting class and also enrolled in online classes to hone
-        her skills. She gets inspiration for her paintings by just observing
-        what’s around her in nature, her travels, people she’s met and people
-        she admires. Painting women is her favorite subject. She has always been
-        attracted to bright, bold colors and you can view that in most of her
-        paintings. <br />
+        Kelly grew up in a very country-like setting in Ohio. 
+        She comes from a family of artists; her mother and her two brothers. 
+        Her mother is the one that gave her the love of art and it has always in the back of her mind somewhere hiding.<br /><br />
+        Fast forward to 2018 and that’s when she decided to put that notion into motion! Kelly picked up a paintbrush for the very first time and began painting. No real formal training, she decided to enroll in a local painting class and also enrolled in online classes to hone her skills. She gets inspiration for her paintings by observing what’s around her in nature, her travels, people she’s met and people she admires. Kelly’s paintings can be described as polychromatic, using an array of colors and detail. Painting women is her favorite subject, but she is also known for the animals she paints (loves elephants!), landscapes, and flowers. She always had been attracted to bright, bold colors and you can view that in most of her paintings. 
         <br />
-        Kelly has lived in Boulder County for over 30 years. She lives with her
-        husband and her ornery rescue dog, Max and her princess, Penelope, a
-        King Charles Cavalier. She loves hiking everyday with her dogs, curling
-        up with a book and binging the latest shows with her husband.
-        <br /><br />
+        <br />
+        Kelly’s paintings have been on exhibit at R Gallery + Wine Bar (now known as Open Studios Gallery Downtown) in Boulder, Co. Also she has displayed at Don Woodard Art Gallery in Broomfield, Co.
+        <br />
+        <br />
+        Kelly has lived in Boulder County for over 30 years. She lives with her husband and her ornery rescue dog, Max and her princess, Penelope, a King Charles Cavalier. She loves hiking everyday with her dogs, curling up with a book and binging the latest shows with her husband. 
+        <br />
+        <br />
         <strong class="font-semibold">
           Commissions welcomed. Please contact me if I can create something
           colorful for you.
